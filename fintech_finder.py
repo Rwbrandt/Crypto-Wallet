c982e763f48b63dfeb651eb13c861432cc61c14d
Rwbrandt <brandt.rogerw@gmail.com>
2022-02-17 15:10:51 -0500

    Candidate Image Upload

--- fintech_finder.py
+++ fintech_finder.py
@@ -38,7 +38,7 @@ from crypto_wallet import generate_account, get_balance, send_transaction
 # Database of Fintech Finder candidates including their name, digital address, rating and hourly cost per Ether.
 # A single Ether is currently valued at $1,500
 candidate_database = {
-    "Lane": ["Lane", "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0", "4.3", .20, "Images/lane.jpeg"],
+    "Lane": ["Lane", "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0", "4.3", .20, "Images/lane.jpg"],
     "Ash": ["Ash", "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396", "5.0", .33, "Images/ash.jpeg"],
     "Jo": ["Jo", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.7", .19, "Images/jo.jpeg"],
     "Kendall": ["Kendall", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.1", .16, "Images/kendall.jpeg"]
@@ -127,25 +127,6 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Step 2: Sign and Execute a Payment Transaction
 
 # Complete the following steps:
-
-# 2. Now that the application can calculate a candidate’s wage, write the code
-# that will allow a customer (you, in this case) to send an Ethereum blockchain
-# transaction that pays the hired candidate. To accomplish this, locate the
-# code that reads `if st.sidebar.button("Send Transaction")`. You’ll need to
-# add logic to this `if` statement that sends the appropriate information to
-# the `send_transaction` function (which you imported from the `crypto_wallet`
-# script file). Inside the `if` statement, add the following functionality:
-
-    # * Call the `send_transaction()` function and pass it three parameters:
-        # - Your Ethereum `account` information. (Remember that this `account`
-        # instance was created when the `generate_account` function was called.)
-        #  From the `account` instance, the application will be able to access the
-        #  `account.address` information that is needed to populate the `from` data attribute in the raw transaction.
-        #- The `candidate_address` (which will be created and identified in the
-        # sidebar when a customer selects a candidate). This will populate the `to` data attribute in the raw transaction.
-        # - The `wage` value. This will be passed to the `toWei` function to
-        # determine the wei value of the payment in the raw transaction.
-
     # * Save the transaction hash that the `send_transaction` function returns
     # as a variable named `transaction_hash`, and have it display on the application’s web interface.
 
@@ -167,11 +148,10 @@ st.sidebar.write(wage)
 if st.sidebar.button("Send Transaction"):
 
     # @TODO
-    # Call the `send_transaction` function and pass it 3 parameters:
-    # `account`,  `candidate_address`, and  `wage` as parameters
+    # Call the `send_transaction` function and pass it 3 parameters: `account`,  `candidate_address`, and  `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
     trans_send = send_transaction(account, candidate_address, wage)
-    transaction_hash = 
+    transaction_hash = trans_send
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
@@ -182,8 +162,7 @@ if st.sidebar.button("Send Transaction"):
     # Celebrate your successful payment
     st.balloons()
 
-# The function that starts the Streamlit application
-# Writes FinTech Finder candidates to the Streamlit page
+# The function that starts the Streamlit application, Writes FinTech Finder candidates to the Streamlit page
 get_people()
 
 ################################################################################
